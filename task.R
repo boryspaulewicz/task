8b5c7b4f3f35bc1707ac8d38492be7ca04293ea5
Borysław Paulewicz <borys.paulewicz@gmail.com>
2016-11-30 18:24:35 +0100

    Teraz wiek musi być dwucyfrowy

--- task.R
+++ task.R
@@ -470,7 +470,7 @@ gui.user.data = function(){
         }
         msg = ''
         if(gender$active < 0)msg = paste(msg, 'Nie wybrano płci\n', sep = '')
-        if(length(grep('[0-9][0-9]', gender$age) == 0)msg = paste(msg, 'Nie podano poprawnie wieku\n', sep = '')
+        if(length(grep('^[0-9][0-9]$', gender$age) == 0)msg = paste(msg, 'Nie podano poprawnie wieku\n', sep = '')
         if(is.na(as.numeric(age$text)))msg = paste(msg, 'Błąd w polu wieku\n', sep = '')
         if(length(grep('^[a-z][a-z][0-9][0-9][0-9][0-9]$', name$text)) == 0)msg =
             paste(msg, 'Identyfikator musi się składac z dwóch liter (inicjałów),\n dnia (dwie cyfry) i miesiąca (dwie cyfry) urodzenia\n', sep = '')
